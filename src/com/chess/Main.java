060f907688b11534d6f417ddb6f78102012d5bcb
VLAD <mateiaass@gmail.com>
2021-04-21 14:50:42 +0300

    vlad - 4.21.2021

--- src/com/chess/Main.java
+++ src/com/chess/Main.java
@@ -1,8 +1,12 @@
 package com.chess;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //System.in.read(new byte[System.in.available()]);
         Game.getInstance().readInput();
 
     }
--- src/com/chess/Rook.java
+++ src/com/chess/Rook.java
@@ -1,4 +1,7 @@
 package com.chess;
+
+import java.util.ArrayList;
+
 /*
     Clasa specifica piesei "Tura"
  */
@@ -10,7 +13,59 @@ public class Rook extends Piece {
     }
 
     public void generateMoves() {
+        freeMoves = new ArrayList<>();
+        captureMoves = new ArrayList<>();
+        int x = coordinate.getIntX();
+        int y = coordinate.getY();
+        Board board = Board.getInstance();
 
+        if (x + 1 <= 8) {
+            for (int i = x + 1; i <= 8; i++) {
+                int type = board.isEmpty(board.getCoordinates(i, y), color);
+                if (type == Move.FREE) {
+                    freeMoves.add(board.getCoordinates(i, y));
+                }
+                if (type == Move.CAPTURE) {
+                    captureMoves.add(board.getCoordinates(i, y));
+                }
+            }
+        }
+
+        if (x - 1 >= 0) {
+            for (int i = x - 1; i >= 0; i--) {
+                int type = board.isEmpty(board.getCoordinates(i, y), color);
+                if (type == Move.FREE) {
+                    freeMoves.add(board.getCoordinates(i, y));
+                }
+                if (type == Move.CAPTURE) {
+                    captureMoves.add(board.getCoordinates(i, y));
+                }
+            }
+        }
+
+        if (y + 1 <= 8) {
+            for (int i = y + 1; i <= 8; i++) {
+                int type = board.isEmpty(board.getCoordinates(x, i), color);
+                if (type == Move.FREE) {
+                    freeMoves.add(board.getCoordinates(x, i));
+                }
+                if (type == Move.CAPTURE) {
+                    captureMoves.add(board.getCoordinates(x, i));
+                }
+            }
+        }
+
+        if (y - 1 >= 0) {
+            for (int i = y - 1; i >= 0; i--) {
+                int type = board.isEmpty(board.getCoordinates(x, i), color);
+                if (type == Move.FREE) {
+                    freeMoves.add(board.getCoordinates(x, i));
+                }
+                if (type == Move.CAPTURE) {
+                    captureMoves.add(board.getCoordinates(x, i));
+                }
+            }
+        }
     }
 
     @Override
